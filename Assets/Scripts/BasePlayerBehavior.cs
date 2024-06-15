6356c3df62ea90d5014e00482bfbc6d89b360de5
edgardanielgd <edgardanielgd123@gmail.com>
2024-06-15 12:32:30 -0500

    dev: Created simple network behaviors for two objects

--- Assets/Scripts/BasePlayerBehavior.cs
+++ Assets/Scripts/BasePlayerBehavior.cs
@@ -1,69 +1,23 @@
 using Unity.Netcode;
 using UnityEngine;
 
-namespace YtanWars
+public class BasePlayerBehavior : NetworkBehaviour
 {
-    public class BasePlayerBehavior : MonoBehaviour
+    private void Update()
     {
-        private NetworkManager m_NetworkManager;
+        if (!IsOwner) return;
 
-        void Awake()
-        {
-            m_NetworkManager = GetComponent<NetworkManager>();
-        }
+        Vector3 moveDir = new Vector3(0,0,0);
 
-        void OnGUI()
-        {
-            GUILayout.BeginArea(new Rect(10, 10, 300, 300));
-            if (!m_NetworkManager.IsClient && !m_NetworkManager.IsServer)
-            {
-                StartButtons();
-            }
-            else
-            {
-                StatusLabels();
+        if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
+        if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
+        if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+        if (Input.GetKey(KeyCode.E)) moveDir.y = +1f;
+        if (Input.GetKey(KeyCode.Q)) moveDir.y = -1f;
 
-                SubmitNewPosition();
-            }
+        float moveSpeed = 3f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
 
-            GUILayout.EndArea();
-        }
-
-        static void StartButtons()
-        {
-            if (GUILayout.Button("Host")) m_NetworkManager.StartHost();
-            if (GUILayout.Button("Client")) m_NetworkManager.StartClient();
-            if (GUILayout.Button("Server")) m_NetworkManager.StartServer();
-        }
-
-        static void StatusLabels()
-        {
-            var mode = m_NetworkManager.IsHost ?
-                "Host" : m_NetworkManager.IsServer ? "Server" : "Client";
-
-            GUILayout.Label("Transport: " +
-                m_NetworkManager.NetworkConfig.NetworkTransport.GetType().Name);
-            GUILayout.Label("Mode: " + mode);
-        }
-
-        static void SubmitNewPosition()
-        {
-            if (GUILayout.Button(m_NetworkManager.IsServer ? "Move" : "Request Position Change"))
-            {
-                if (m_NetworkManager.IsServer && !m_NetworkManager.IsClient)
-                {
-                    foreach (ulong uid in m_NetworkManager.ConnectedClientsIds)
-                        m_NetworkManager.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<BasePlayer>().Move();
-                }
-                else
-                {
-                    var playerObject = m_NetworkManager.SpawnManager.GetLocalPlayerObject();
-                    var player = playerObject.GetComponent<BasePlayer>();
-                    player.Move();
-                }
-            }
-        }
     }
 }
-
-
